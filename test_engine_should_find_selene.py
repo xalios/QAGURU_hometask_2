18e83481bb9a62a6eacb3aadbd79586f45ce1669
Xalios <mastep@yandex.ru>
2023-06-12 01:11:52 +0300

    Remove duplicates from old structure

--- test_engine_should_find_selene.py
+++
@@ -1,8 +0,0 @@
-from selene.support.shared import browser
-from selene import be, have
-
-
-def test_serp_title_has_changed():
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))

37ace14da70e2e37343563518591aad41cd7211d
Xalios <mastep@yandex.ru>
2023-06-12 17:58:51 +0300

    add selene tests

---
+++ tests/test_serp_selene.py
@@ -0,0 +1,21 @@
+import pytest
+from selene import browser
+from selene.support.conditions import be, have
+
+
+@pytest.fixture
+def set_window_size():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+
+def test_selene_repo_presented_in_search_results(set_window_size):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_empty_serp(set_window_size):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('vjnfirnijnpqjwnej').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))

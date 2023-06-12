576fa8717c05693e39cd2521370400e13f5204ed
Xalios <mastep@yandex.ru>
2023-06-12 18:01:35 +0300

    move start page opening to fixture

--- tests/test_serp_selene.py
+++ tests/test_serp_selene.py
@@ -7,15 +7,14 @@ from selene.support.conditions import be, have
 def set_window_size():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.open('https://google.com')
 
 
 def test_selene_repo_presented_in_search_results(set_window_size):
-    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
 def test_empty_serp(set_window_size):
-    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('vjnfirnijnpqjwnej').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
